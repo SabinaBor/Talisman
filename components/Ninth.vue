eed3cee21cef96d54e3297057597ae68e98231d2
SabinaBor <sboranbayeva@gmail.com>
2022-03-15 08:33:14 +0600

    fixes

--- components/Ninth.vue
+++ components/Ninth.vue
@@ -1,6 +1,54 @@
 <template>
     <div id="eightth" class="main__right__ninth full-height">
-        <span class="title">Контакты</span>
+        <div class="contacts">
+            <div class="contacts__top">
+                <h3>ЗАКАЖИТЕ ЗВОНОК!</h3>
+                <p>Мы свяжемся с вами и ответим на все ваши вопросы</p>
+                <div class="contacts__top__form">
+                    <input type="text" v-model="name" placeholder="Имя" />
+                    <input type="text" v-model="phone_number" placeholder="Номер телефона: +7" />
+                    <button class="contacts__top__form__btn">
+                        <img src="../assets/images/arrow-right_yellow.svg" alt="image" />
+                    </button>
+                </div>
+            </div>
+            <div class="contacts__middle">
+                <div class="contacts__middle__left">
+                    <p>
+                        НАШИ КОНТАКТЫ
+                    </p>
+                    <p>
+                        моб: +7 777 393 2222
+                    </p>
+                    <p>
+                        тел: +7 724 393 2222
+                    </p>
+                    <p>
+                        email: talisman@taraz.kz
+                    </p>
+                </div>
+                <div class="contacts__middle__right">
+
+                </div>
+            </div>
+            <div class="contacts__bottom">
+                <div class="contacts__bottom__links">
+                    <div class="facebook-link">
+                        <img src="../assets/images/facebook.svg" alt="facebook" />
+                        <div class="arrow-right">
+                            <img src="../assets/images/arrow-right_white.svg" alt="arrow" />
+                        </div>
+                    </div>
+                    <div class="instagram-link">
+                        <img src="../assets/images/instagram.svg" alt="instagram" />
+                        <div class="arrow-right">
+                            <img src="../assets/images/arrow-right_white.svg" alt="arrow" />
+                        </div>
+                    </div>
+                </div>
+                <p>©2021-2022 TALISMAN RESIDENTIAL COMPLEX. Все права защищены.</p>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,3 +57,4 @@ export default {
     name: "NinthComponent"
 }
 </script>
+
--- components/Seventh.vue
+++ components/Seventh.vue
@@ -1,11 +1,101 @@
 <template>
-    <div id="seventh" class="main__right__seventh full-height">
-        <span class="title">Планировка</span>
+  <div id="seventh" class="main__right__seventh full-height">
+    <div class="main__right__seventh__items">
+      <div class="main__right__seventh__item">
+        <div class="main__right__seventh__item__top">
+          <div class="img__wrapper">
+            <img src="../assets/images/arrow-right.svg" alt="arrow-right" />
+          </div>
+          <div class="apartment-type">
+            <p>ПЛАНИРОВКА</p>
+            <span>2-Х КВ.</span>
+          </div>
+        </div>
+        <div class="main__right__seventh__item__bottom">
+          <p class="white">2-Х КВАРТИР:</p>
+          <p class="gray">БЛОК A: 14</p>
+          <p class="gray">БЛОК B: 14</p>
+          <p class="gray">БЛОК C: 14</p>
+          <p class="white">ПЛОЩАДЬ: от 62 м^2</p>
+          <p class="white">ЦЕНА: от 702 000 тг. за м^2</p>
+        </div>
+      </div>
+      <div class="main__right__seventh__item">
+        <div class="main__right__seventh__item__top">
+          <div class="img__wrapper">
+            <img src="../assets/images/arrow-right.svg" alt="arrow-right" />
+          </div>
+          <div class="apartment-type">
+            <p>ПЛАНИРОВКА</p>
+            <span>3-Х КВ.</span>
+          </div>
+        </div>
+        <div class="main__right__seventh__item__bottom">
+          <p class="white">3-Х КВАРТИР:</p>
+          <p class="gray">БЛОК A: 37</p>
+          <p class="gray">БЛОК B: 37</p>
+          <p class="gray">БЛОК C: 37</p>
+          <p class="white">ПЛОЩАДЬ: от 75 м^2</p>
+          <p class="white">ЦЕНА: от 650 000 тг. за м^2</p>
+        </div>
+      </div>
+      <div class="main__right__seventh__item">
+        <div class="main__right__seventh__item__top">
+          <div class="img__wrapper">
+            <img src="../assets/images/arrow-right.svg" alt="arrow-right" />
+          </div>
+          <div class="apartment-type">
+            <p>ПЛАНИРОВКА</p>
+            <span>4-Х КВ.</span>
+          </div>
+        </div>
+        <div class="main__right__seventh__item__bottom">
+          <p class="white">4-Х КВАРТИР:</p>
+          <p class="gray">БЛОК A: 21</p>
+          <p class="gray">БЛОК B: 21</p>
+          <p class="gray">БЛОК C: 21</p>
+          <p class="white">ПЛОЩАДЬ: от 105 м^2</p>
+          <p class="white">ЦЕНА: от 602 000 тг. за м^2</p>
+        </div>
+      </div>
     </div>
+    <div class="download-plan">Скачать планировку квартир (PDF, 18kb)</div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "SeventhComponent"
-}
+  name: "SeventhComponent",
+};
 </script>
+
+<style lang="scss" scoped>
+.main__right__seventh {
+  &__items {
+    display: flex;
+    margin: 0 auto;
+    width: 100%;
+    padding: 80px 70px;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+  &__item {
+    width: calc(100% / 3 - 70px);
+    &__top {
+      display: flex;
+      border: 1px solid #fff;
+      align-items: center;
+      padding: 20px;
+      .img__wrapper {
+        height: 20px;
+        width: 20px;
+        margin-right: 20px;
+        img {
+          height: 100%;
+          width: 100%;
+        }
+      }
+    }
+  }
+}
+</style>
